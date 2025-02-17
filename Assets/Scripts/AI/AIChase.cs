37df2305f806e5ce217536c6704e083a082231c6
Reignier G. Dahunog <dahunog.295426@dasmarinas.sti.edu.ph>
2025-02-17 20:54:16 +0800

    New Update

--- Assets/Scripts/AI/AIChase.cs
+++ Assets/Scripts/AI/AIChase.cs
@@ -4,13 +4,19 @@ public class AIChase : MonoBehaviour
 {
     public GameObject player;
     public float speed;
-    private float distance;
+    public int health = 3; // Health ng mga batugan na maliliit
+    public bool isBoss = false; // yung boss obviously malaki tapos dapat iba kulay so...
 
+    private float distance;
     private SpriteRenderer spriteRenderer;
 
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        if (isBoss)
+        {
+            health = 100; // gawin nating 100 yung health babaan mo if masakit na sya masyado </3
+        }
     }
 
     void Update()
@@ -28,15 +34,24 @@ public class AIChase : MonoBehaviour
         }
 
         transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
-
-        if (distance < 0.1f)
-        {
-            // Anything for this bi a t c h i
-        }
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player_Projectile"))
+        {
+            TakeDamage();
+        }
+    }
+
+    public void TakeDamage()
+    {
+        health--;
+        if (isBoss)
+        {
+            Debug.Log("Boss hit! Remaining health: " + health);
+        }
+        if (health <= 0)
         {
             Destroy(gameObject);
         }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,314 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 1f;
+
+    private PlayerControls playerControls;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    private Animator myAnimator;
+    private SpriteRenderer mySpriteRender;
+
+    // Projectile
+    [SerializeField] private GameObject arrowObject;
+    private bool canShoot = true;
+    private float shootCooldown = 1f;
+
+    // Movement
+    private Vector2 lastMovementDirection;
+
+    // Player Variables
+    [SerializeField] private int maxHealth = 100;
+    private int currentHealth;
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+        rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRender = GetComponent<SpriteRenderer>();
+        currentHealth = maxHealth;
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void Update()
+    {
+        PlayerInput();
+        fireProjectile();
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            FireContinuousProjectiles(); //pa ni pinindot yung space sheesh god mode attack for testing lang
+        }
+
+        if (Input.GetKey(KeyCode.X))
+        {
+            NormalContinuousProjectiles(); //pa ni pinindot yung x normal attack lang pero 5 bullets
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        AdjustPlayerFacingDirection();
+        Move();
+    }
+
+    private void PlayerInput()
+    {
+        movement = playerControls.Movement.Move.ReadValue<Vector2>();
+
+        if (movement != Vector2.zero)
+        {
+            lastMovementDirection = movement;
+        }
+
+        myAnimator.SetFloat("moveX", movement.x);
+        myAnimator.SetFloat("moveY", movement.y);
+    }
+
+    private void Move()
+    {
+        rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    private void AdjustPlayerFacingDirection()
+    {
+        if (movement.x < 0) { mySpriteRender.flipX = true; }
+        else if (movement.x > 0) { mySpriteRender.flipX = false; }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        Debug.Log("Damage received: " + damage + " Current Health: " + currentHealth);
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log("Player Died!");
+        gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Mob"))
+        {
+            TakeDamage(2);
+        }
+    }
+
+    private void fireProjectile()
+    {
+        if (canShoot && lastMovementDirection != Vector2.zero)
+        {
+            canShoot = false;
+
+            Vector2 fireDirection = lastMovementDirection.normalized;
+
+            GameObject projectile_arrow = Instantiate(arrowObject, transform.position, Quaternion.identity);
+            projectile_arrow.GetComponent<Rigidbody2D>().velocity = fireDirection * 5.0f;
+            Destroy(projectile_arrow, 2f);
+
+            StartCoroutine(ShootCooldown());
+        }
+    }
+
+    private void FireContinuousProjectiles()
+    {
+        if (lastMovementDirection != Vector2.zero)
+        {
+            Vector2 fireDirection = lastMovementDirection.normalized;
+
+            GameObject projectile_arrow = Instantiate(arrowObject, transform.position, Quaternion.identity);
+            projectile_arrow.GetComponent<Rigidbody2D>().velocity = fireDirection * 5.0f; //dito iaadjust yung bilis ng bullets
+            Destroy(projectile_arrow, 2f);
+        }
+    }
+
+    private void NormalContinuousProjectiles()
+    {
+        if (lastMovementDirection != Vector2.zero)
+        {
+            Vector2 fireDirection = lastMovementDirection.normalized;
+
+            GameObject projectile_arrow = Instantiate(arrowObject, transform.position, Quaternion.identity);
+            projectile_arrow.GetComponent<Rigidbody2D>().velocity = fireDirection * 1.0f; //dito iaadjust yung bilis ng bullets
+            Destroy(projectile_arrow, 2f);
+        }
+    }
+
+    private IEnumerator ShootCooldown()
+    {
+        yield return new WaitForSeconds(shootCooldown);
+        canShoot = true;
+    }
+}*/ // uncomment nyo nalang toh then try nyo
+
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 1f;
+
+    private PlayerControls playerControls;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    private Animator myAnimator;
+    private SpriteRenderer mySpriteRender;
+
+    // Projectile
+    [SerializeField] private GameObject arrowObject;
+
+    // Movement
+    private Vector2 lastMovementDirection;
+
+    // Player Variables
+    [SerializeField] private int maxHealth = 100;
+    private int currentHealth;
+
+    private bool isGodModeActive = false;
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+        rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRender = GetComponent<SpriteRenderer>();
+        currentHealth = maxHealth;
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+        StartCoroutine(TickingFunction());
+    }
+
+    private void Update()
+    {
+        PlayerInput();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (lastMovementDirection != Vector2.zero)
+            {
+                GameObject projectile_arrow = Instantiate(arrowObject, transform.position, Quaternion.identity);
+                projectile_arrow.GetComponent<Rigidbody2D>().linearVelocity = lastMovementDirection.normalized * 2.0f;
+                Destroy(projectile_arrow, 2f);
+            }
+
+            fireProjectile();
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            isGodModeActive = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.X))
+        {
+            isGodModeActive = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        AdjustPlayerFacingDirection();
+        Move();
+    }
+
+    private void PlayerInput()
+    {
+        movement = playerControls.Movement.Move.ReadValue<Vector2>();
+
+        if (movement != Vector2.zero)
+        {
+            lastMovementDirection = movement;
+        }
+
+        myAnimator.SetFloat("moveX", movement.x);
+        myAnimator.SetFloat("moveY", movement.y);
+    }
+
+    private void Move()
+    {
+        rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    private void AdjustPlayerFacingDirection()
+    {
+        if (movement.x < 0) { mySpriteRender.flipX = true; }
+        else if (movement.x > 0) { mySpriteRender.flipX = false; }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        Debug.Log("Damage received: " + damage + " Current Health: " + currentHealth);
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log("Player Died!");
+        gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Mob"))
+        {
+            TakeDamage(10);
+        }
+    }
+
+    private IEnumerator TickingFunction()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.2f);
+            if (isGodModeActive && lastMovementDirection != Vector2.zero)
+            {
+                GodMode();
+            }
+            fireProjectile();
+        }
+    }
+
+    private void fireProjectile()
+    {
+        GameObject projectile_arrow = Instantiate(arrowObject, transform.position, Quaternion.identity);
+        projectile_arrow.GetComponent<Rigidbody2D>().linearVelocity = lastMovementDirection.normalized * 2.0f;
+        Destroy(projectile_arrow, 2f);
+    }
+
+    private void GodMode()
+    {
+        if (lastMovementDirection != Vector2.zero)
+        {
+            Vector2 fireDirection = lastMovementDirection.normalized;
+
+            GameObject projectile_arrow = Instantiate(arrowObject, transform.position, Quaternion.identity);
+            projectile_arrow.GetComponent<Rigidbody2D>().velocity = fireDirection * 5.0f;
+            Destroy(projectile_arrow, 2f);
+        }
+    }
+}*/ //try nyo rin toh pag mag sasave kayo press nyo lang yung NO 
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -64,6 +375,7 @@ public class PlayerController : MonoBehaviour
     private void PlayerInput()
     {
         movement = playerControls.Movement.Move.ReadValue<Vector2>();
+
         if (movement != Vector2.zero)
         {
             lastMovementDirection = movement;
@@ -78,12 +390,10 @@ public class PlayerController : MonoBehaviour
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
     }
 
-    private void AdjustPlayerFacingDirection()           
+    private void AdjustPlayerFacingDirection()
     {
-        Vector3 mousePos = Input.mousePosition;
-        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
-
-        if (mousePos.x < playerScreenPoint.x) {mySpriteRender.flipX = true;} else {mySpriteRender.flipX = false;}
+        if (movement.x < 0) { mySpriteRender.flipX = true; }
+        else if (movement.x > 0) { mySpriteRender.flipX = false; }
     }
 
     public void TakeDamage(int damage)
