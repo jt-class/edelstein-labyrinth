1e9da6a96510efc03a260326fb0dc72236d878b6
NFernandez02 <waltuhmarkson@gmail.com>
2025-02-23 10:12:18 +0800

    Added Healthbar

---
+++ Assets/Scripts/UI/HealthBarUI.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarUI : MonoBehaviour
+{
+    [SerializeField] private PlayerController playerController;
+    [SerializeField] private Image health;
+    //Add Events on player to make this work. 
+    //Di ko na ginalaw naka comment ung buong PlayerController e hahah
+    private void Awake()
+    {
+        
+    }
+}
